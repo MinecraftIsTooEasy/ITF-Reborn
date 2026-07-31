332ae2141607eb50f75ce00e545cc19aedf7b4f4
Xy_Luce <155866943+ysesiq@users.noreply.github.com>
2026-07-31 18:30:17 +0800

    Reimplement Shop compat

---
+++ src/main/java/net/oilcake/mitelros/compat/NewShopPluginImpl.java
@@ -0,0 +1,13 @@
+package net.oilcake.mitelros.compat;
+
+import com.inf1nlty.newshop.api.ShopPlugin;
+import com.inf1nlty.newshop.api.ShopRegistry;
+import net.oilcake.mitelros.registry.item.Items;
+
+public class NewShopPluginImpl implements ShopPlugin {
+    @Override
+    public void register(ShopRegistry registry) {
+        registry.addItem(Items.agave, 0.0, 1.0);
+        registry.addItem(Items.glowberries, 0.0, 1.0);
+    }
+}
